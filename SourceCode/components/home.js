78e2a4d72626ab9a5760715992b5d1ffee3f376c
Boobosua <nttrung95@yahoo.com>
2016-11-18 19:27:05 +0700

    + Add home.js + Add tab bar + Add DrawerLayout

---
+++ SourceCode/components/home.js
@@ -0,0 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import DrawerLayout from 'react-native-drawer-layout';
+import * as Animatable from 'react-native-animatable';
+import Collapsible from 'react-native-collapsible';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Navigator,
+    Dimensions,
+    TouchableNativeFeedback,
+    TouchableOpacity,
+    Image,
+    InputText,
+    ScrollView
+} from 'react-native';
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+
+var {height, width} = Dimensions.get('window');
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    _changeState(check) {
+        this.setState({check: check})
+    }
+
+    _openMenu() {
+        if (this.state.check == true) {
+            this.refs['DRAWER_REF'].openDrawer();
+            this.setState({check: false})
+        } else {
+            this.refs['DRAWER_REF'].closeDrawer();
+            this.setState({check: true})
+        }
+    }
+
+    _navigationView() {
+        return (
+            <View></View>
+        );
+    }
+
+    render() {
+        return (
+            <DrawerLayout ref={'DRAWER_REF'} drawerWidth={width * 3 / 4 + 40} onDrawerOpen={() => {
+                this._changeState(false)
+            }} onDrawerClose={() => {
+                this._changeState(true)
+            }} drawerPosition={DrawerLayout.positions.Left} renderNavigationView={() => this._navigationView()}>
+                <View style={styles.home_layout}>
+                    <View style={styles.bar_layout}>
+                        <View></View>
+                        <View></View>
+                    </View>
+                    <View style={styles.page_layout}></View>
+                </View>
+            </DrawerLayout>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    home_layout: {
+        flex: 1,
+        backgroundColor: '#FAFAFA',
+        flexDirection: 'column'
+    },
+    bar_layout: {
+        flex: 0.1,
+        backgroundColor: '#212121',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    leftbar: {
+        flex: 0.5,
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    rightbar: {
+        flex: 0.5,
+        alignItems: 'flex-end',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    page_layout: {
+        flex: 0.9,
+        backgroundColor: '#e8e8e8'
+    }
+});
+
+module.exports = Home;
--- SourceCode/index.android.js
+++ SourceCode/index.android.js
@@ -4,50 +4,18 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View, Navigator} from 'react-native';
+
+var Home = require('./components/home.js');
 
 export default class ArtDeco extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (<Navigator initialRoute={{
+            name: 'Home',
+            component: Home
+        }} renderScene= {(route, navigator) => { if (route.component){ return React.createElement(route.component, {navigator, passProps: route.props}); } }}/>);
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ArtDeco', () => ArtDeco);
